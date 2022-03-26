e6fe6a7657376dd3651e1330ceb9a866f520c9d4
lewetzk <66308608+lewetzk@users.noreply.github.com>
2022-03-26 12:02:04 +0100

    Add files via upload

--- project.r
+++ project.r
@@ -13,25 +13,53 @@ require("tm")
 load("lexika/sentiWS.RData")
 sentiment.lexikon.sentiws <- dictionary(list(positive = positive.woerter.senti, negative = negative.woerter.senti))
 
-# loading in csv files 
-files = list.files(pattern="*.csv")
+# loading in Junge Freiheit csv files 
+jf_files = list.files(pattern="jf-[0-9][0-9]?.csv")
+# loading in Tagesschau csv files
+ts_files = list.files(pattern="ts-[0-9][0-9]?.csv")
 # First apply read.csv, then rbind
-myfiles = do.call(rbind, lapply(files, function(x) read.csv(x, sep=';', stringsAsFactors = FALSE)))
+
+make_corp <- function(df_name, col_name) {
+  df_column <- df_name[col_name]
+  column_corpus <- corpus(df_column, text_field = col_name)
+  return(column_corpus)
+}
+
+jf_dfs = do.call(rbind, lapply(jf_files, function(x) read.csv(x, sep=';', stringsAsFactors = FALSE)))
 # dividing into different dfs for later processing
-head = myfiles["HEAD"]
-head$source <- "Junge Freiheit"
-subhead = myfiles["SUBHEAD"]
-text = myfiles["TEXT"]
+# jf_head$source <- "Junge Freiheit"
+# jf_subhead$source <- "Junge Freiheit"
+# jf_text$source <- "Junge Freiheit"
 # turning dfs into corpus objects
-head_corpus <- corpus(head, text_field = "HEAD")
-head_corpus
+jf_head_corpus <- make_corp(jf_dfs, "HEAD")
+jf_subhead_corpus <- make_corp(jf_dfs, "SUBHEAD")
+jf_text_corpus <- make_corp(jf_dfs, "TEXT")
+
+ts_dfs = do.call(rbind, lapply(jf_files, function(x) read.csv(x, sep=';', stringsAsFactors = FALSE)))
+# dividing into different dfs for later processing
+# jf_head$source <- "Junge Freiheit"
+# jf_subhead$source <- "Junge Freiheit"
+# jf_text$source <- "Junge Freiheit"
+# turning dfs into corpus objects
+ts_head_corpus <- make_corp(ts_dfs, "HEAD")
+ts_subhead_corpus <- make_corp(ts_dfs, "SUBHEAD")
+ts_text_corpus <- make_corp(ts_dfs, "TEXT")
+
+# function for calculating total amount of 'emotional' words in a corpus
+make_sentiws_weights <- function(corpus_name) {
+  corpus_dfm <- dfm(corpus_name, dictionary = sentiment.lexikon.sentiws)
+  # make document feature matrix with sentiws 
+  corpus_weight <- dfm_weight(corpus_dfm)
+  dfm_converted <- convert(corpus_weight, "data.frame")
+  emwords_sum <- sum(dfm_converted$negative) + sum(dfm_converted$positive)
+  # convert dfm to data frame and summarize occurrences of negative and positive words
+  return(emwords_sum)
+}
+
+emwords_head <- make_sentiws_weights(jf_head_corpus)
+emwords_subheads <- make_sentiws_weights(jf_subhead_corpus)
+emwords_text <- make_sentiws_weights(jf_text_corpus)
+
+total_tokens <- sum(quanteda::ntoken(jf_text_corpus))
 
 
-# making document feature matrix of headlines
-meine.dfm.head <- dfm(head_corpus, dictionary = sentiment.lexikon.sentiws)
-# weighing features (negative and positive)
-meine.dfm.head.weight <- dfm_weight(meine.dfm.head)
-topfeatures(meine.dfm.head.weight)
-# converting dfm to dataframe to summarize total amounts of "emotional" words
-total <- convert(meine.dfm.head.weight, "data.frame")
-sum(total$negative)
